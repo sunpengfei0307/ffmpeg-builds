8f83629dbd35cd1472d34e2cd69e3805b4bd4b8f
sunpengfei <root@hongkong-globalswitch-10-191-6-5.idc.qiyi.domain>
2026-07-14 14:45:14 +0800

    fetch and init ffmpeg-8.1.2 (pure)

---
+++ ffmpeg/ffmpeg-8.1.2/libavfilter/cuda/load_helper.d
@@ -0,0 +1,20 @@
+libavfilter/cuda/load_helper.o: libavfilter/cuda/load_helper.c config.h \
+ libavutil/hwcontext.h libavutil/buffer.h libavutil/frame.h \
+ libavutil/avutil.h libavutil/common.h libavutil/attributes.h \
+ libavutil/error.h libavutil/macros.h libavutil/avconfig.h \
+ libavutil/version.h libavutil/intmath.h libavutil/x86/intmath.h \
+ libavutil/internal.h libavutil/libm.h libavutil/mathematics.h \
+ libavutil/rational.h libavutil/intfloat.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/samplefmt.h libavutil/hwcontext_cuda_internal.h \
+ compat/cuda/dynlink_loader.h libavutil/log.h compat/w32dlfcn.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/ffnvcodec/dynlink_loader.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/ffnvcodec/dynlink_cuda.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/ffnvcodec/dynlink_nvcuvid.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/ffnvcodec/dynlink_cuviddec.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/ffnvcodec/nvEncodeAPI.h \
+ libavutil/hwcontext_cuda.h libavutil/cuda_check.h libavutil/mem.h \
+ libavutil/zlib_utils.h libavutil/mem.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/zlib.h \
+ /data/sunpf/ffmpeg-builds/build/release/include/zconf.h \
+ libavfilter/cuda/load_helper.h
